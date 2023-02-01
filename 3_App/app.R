688e4fab1e06cd9a89b1ba7b1f6c72a2a88e9f6c
Rachel <rachel_slimovitch@brown.edu>
2023-02-01 12:39:59 -0500

    edited- need help w/server

--- 3_App/app.R
+++ 3_App/app.R
@@ -1,6 +1,6 @@
 #************************************* Shiny App Code *************************************#
-#                                                                                          #
-#                                                                                          #
+# Note: this won't work right now                                                          #
+# Built it out to connect to a csv file that is df in 2_years_saved.R                      #
 #                                                                                          #
 #******************************************************************************************#
 
@@ -8,7 +8,10 @@
 
 # source model code
 here::i_am("3_App/app.R")
-#source("2_years_saved.R") not accessing this yet- when I do I get an error
+source(here("3_App", "2_years_saved.R"))
+
+#instead of sourcing this code, could I just export the df to a csv? Called app_data
+#app_data<- read.csv("file name here", stringsAsFactors=FALSE)
 
 # libraries
 library(shiny)
@@ -22,6 +25,8 @@ library(htmltools)
 
 # Define UI:
 ui <- fluidPage(
+  #Theme
+  theme=shinytheme("simplex"),
   
   # Application title
   titlePanel("Model-based analysis of controlled human infection studies for a hepatitis C vaccine"),
@@ -32,12 +37,12 @@ ui <- fluidPage(
       #tabsetPanel(
         
         # MAIN PARAMETERS
-      #  tabPanel("Main", fluid=TRUE,
+        tabPanel("Main", fluid=TRUE,
                  
                  h4("Vaccine information"),
                  h5("These sliders describe describe the efficacy and uptake of a vaccine, where 1 is 100%."),
                  sliderInput("d", "Vaccine uptake fraction", min=.1, max=9, value=.23, step = 0.05),                               
-                 sliderInput("e", "Vccine efficacy fraction", min=.5, max=.9, value=.7, step = 0.05),  
+                 sliderInput("e", "Vaccine efficacy fraction", min=.5, max=.9, value=.7, step = 0.05),  
                  
                  h4("Trial information"),
                  h5("These sliders describe challenge trial inputs."),
@@ -63,7 +68,7 @@ ui <- fluidPage(
                  #               label = 'Download estimates',
                   #              class= "mybutton")
                  
-        ),
+        )),
         
     
     # Show the output
@@ -77,25 +82,52 @@ ui <- fluidPage(
 )))
 
 # Define server logic required to output values
-server <- function(input, output) {
+server <- function(input, output, session) {
+  filtered<- reactive({
+    app_data %>%
+      filter(p == input$p,
+             t == input$t,
+             e == input$y,
+             d == input$d
+             #i == input$i don't have this assigned
+             )
+  })
   
-   output$trial_infections<-renderText({
-     paste("The expected number of trial infections is", input$t) #incorrect
-    })
+  #OUTPUT: Take that filtered row from dataset
+  output$trial_infections<-renderText({
+    paste("The expected number of trial infections is", input$t) #incorrect
+  })
   
-    output$infections_averted_undiscounted<-renderText({
-      paste("The expected infections averted (undiscounted) is", input$t) #incorrect
-    })
-    
-    output$infections_averted_discounted<-renderText({
-      paste("The expected infections averted (discounted) is", input$e) #incorrect
-    })
-    
-    output$br_ratio <- renderText({
-      paste("The benefit-risk ratio is",input$t) #this is not correct right now- need to connect to ratio in 2_years_saved
-    })
-    
-}
+  output$infections_averted_undiscounted<-renderText({
+    paste("The expected infections averted (undiscounted) is", input$t) #incorrect
+  })
+  
+  output$infections_averted_discounted<-renderText({
+    paste("The expected infections averted (discounted) is", filtered$infs) #is this how I write it?
+  })
+  
+  output$br_ratio <- renderText({
+    paste("The benefit-risk ratio is",filtered$br_ratio) #is this how I write it?
+  })
+
+
+  
+  #IF I FOLLOWED THE CONTACT TRACING APP CODE:
+  #INPUTS: other idea
+  #observeEvent(input$file, { #confused what file should be referring to. No slider called file
+    #if file isn't empty:
+   # if(!is.null(inFile)){
+      #load inputs
+    #  uploaded_inputs<-read.csv(inFile$datapath)
+      #update each input
+      
+   # }
+  #})
+#}
+  #MAKE INPUT TABLE
+  #MODEL
+  #WRITE TEXT
+#}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
